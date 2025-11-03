efee4026c79fd9ee6e46f1ca91126a4c4e39be3b
Sadiq Hayatuddeen <svdiqhayatuddeen@gmail.com>
2025-11-03 14:00:13 +0100

    added user, link models and schemas

---
+++ backend/core/db/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+from dotenv import load_dotenv
+import os
+
+load_dotenv("/.env")
+
+DATABASE_URL = str(os.getenv("POSTGRES_URL"))
+Base = declarative_base()
+engine = create_async_engine(DATABASE_URL, echo=True)
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+async def init_db():
+    async with engine.begin() as session:
+        yield session
